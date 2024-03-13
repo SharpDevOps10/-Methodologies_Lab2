cf9607caa3f5cea9341d96366353f3faf41cee18
SharpDevOps10 <td090833@gmail.com>
2024-03-14 00:57:20 +0200

    testing: added testcases for monospace and block ANSI

--- tests/markdown-converter-to-ANSI.test.js
+++ tests/markdown-converter-to-ANSI.test.js
@@ -55,3 +55,59 @@ describe('ANSI: italic testcases', () => {
     expect(() => convertMarkdownToHTML(markdown, { format: 'ansi' })).toThrowError('Unclosed tag was found');
   });
 });
+
+describe('ANSI: monospace testcases', () => {
+  test('should convert monospaced text into ANSI reverse mode', () => {
+    const markdown = 'Hello `monospaced world`!';
+    const expectedANSI = 'Hello \x1b[7mmonospaced world\x1b[27m!';
+    expect(convertMarkdownToHTML(markdown, { format: 'ansi' })).toStrictEqual(expectedANSI);
+  });
+
+  test('shouldn\'t convert monospaced signs separated from the text into ANSI reverse mode', () => {
+    const markdown = 'This ` example is not an element of the markup `';
+    const expectedANSI = 'This ` example is not an element of the markup `';
+    expect(convertMarkdownToHTML(markdown, { format: 'ansi' })).toStrictEqual(expectedANSI);
+  });
+
+  test('shouldn\'t interpret single apostrophe sign as an example of monospace markup', () => {
+    const markdown = 'Apostrophe (`) is the sixth solo album and eighteenth in total by Frank Zappa';
+    const expectedANSI = 'Apostrophe (`) is the sixth solo album and eighteenth in total by Frank Zappa';
+    expect(convertMarkdownToHTML(markdown, { format: 'ansi' })).toStrictEqual(expectedANSI);
+  });
+
+  test('should throw an error if monospace markup is not closed', () => {
+    const markdown = 'This example will throw an `error';
+    expect(() => convertMarkdownToHTML(markdown, { format: 'ansi' })).toThrowError('Unclosed tag was found');
+  });
+});
+
+describe('ANSI: preformatted block testcases', () => {
+  test('should convert preformatted block into HTML text with <pre></pre> tag', () => {
+    const markdown = '```\nEverybody\'s Got Something to Hide Except Me and My Monkey\n```';
+    const expectedANSI = '\x1b[7mEverybody\'s Got Something to Hide Except Me and My Monkey\x1b[27m';
+    expect(convertMarkdownToHTML(markdown, { format: 'ansi' })).toStrictEqual(expectedANSI);
+  });
+
+  test('shouldn\'t interpret formatting signs as an example of markup inside preformatted block', () => {
+    const markdown = '```\nThis one\'s _optimistic_ \nThis one **went** to market\n```';
+    const expectedANSI = '\x1b[7mThis one\'s _optimistic_ \nThis one **went** to market\x1b[27m';
+    expect(convertMarkdownToHTML(markdown, { format: 'ansi' })).toStrictEqual(expectedANSI);
+  });
+
+  test('shouldn\'t interpret unclosed formatting signs as an example of unclosed tags inside preformatted block', () => {
+    const markdown = '```\nBut **listen to _her daddy\'s song \nDon\'t stay `out to long\n```';
+    const expectedANSI = '\x1b[7mBut **listen to _her daddy\'s song \nDon\'t stay `out to long\x1b[27m';
+    expect(convertMarkdownToHTML(markdown, { format: 'ansi' })).toStrictEqual(expectedANSI);
+  });
+
+  test('shouldn\'t interpret nested formatting signs as an example of nested tags inside preformatted block', () => {
+    const markdown = '```\nActing **_funny_**, but I don\'t _`know`_ why \nExcuse me while I kiss the sky\n```';
+    const expectedANSI = '\x1b[7mActing **_funny_**, but I don\'t _`know`_ why \nExcuse me while I kiss the sky\x1b[27m';
+    expect(convertMarkdownToHTML(markdown, { format: 'ansi' })).toStrictEqual(expectedANSI);
+  });
+
+  test('should throw an error if preformatted block is not closed', () => {
+    const markdown = '```\nThis is the end \nBeautiful friend \nThis is the end \nMy only friend\n';
+    expect(() => convertMarkdownToHTML(markdown, { format: 'ansi' })).toThrowError('Unclosed tag was found');
+  });
+});
